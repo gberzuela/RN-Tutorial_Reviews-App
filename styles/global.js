48876ec3e3f4ab79da3b71a824b04e460b258b9d
Gerald Lou Berzuela <berzuelagerald3348@gmail.com>
2021-01-02 09:52:43 -0800

    Lesson 19 - React Navigation Setup

--- styles/global.js
+++ styles/global.js
@@ -3,7 +3,7 @@ import { StyleSheet } from 'react-native';
 export const globalStyles = StyleSheet.create({
 	container: {
 		flex: 1,
-		padding: 20,
+		padding: 24,
 	},
 	paragraph: {
 		marginVertical: 8,
